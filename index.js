c51d4d312a99334a473fb9c07b6193512e787108
Noncedo21 <132228442+Noncedo21@users.noreply.github.com>
2024-09-13 23:16:18 +0200

    Updates js

--- index.js
+++ index.js
@@ -15,18 +15,25 @@ let timeHour = now.getHours();
 let timeMin = now.getMinutes();
 let currentTime = document.querySelector(".time");
 currentTime.innerHTML = `${timeHour}:${timeMin}`;
-
-let apiKey = "cca34be40btf00a3b62f839750cc64eo";
 let citySearch = document.querySelector(".search-text");
-let city = citySearch.value;
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+let apiKey = "cca34be40btf00a3b62f839750cc64eo";
 
 function Searching(event) {
   event.preventDefault();
+
+  let city = citySearch.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
   let currentCity = document.querySelector(".city");
   currentCity.innerHTML = `${citySearch.value}`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function displayTemp(response) {
+  let temperatureCurrent = document.querySelector(".temperature");
+
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureCurrent.innerHTML = `${temperature}`;
 }
 let submitSearch = document.querySelector(".search-button");
 submitSearch.addEventListener("click", Searching);
-axios.get(apiUrl).then(Searching);
