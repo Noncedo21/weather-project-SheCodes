cc36899f76fcb94a7c855ead00d23ca200243b72
Noncedo21 <132228442+Noncedo21@users.noreply.github.com>
2024-09-13 22:39:56 +0200

    Updated js

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ let timeHour = now.getHours();
 let timeMin = now.getMinutes();
 let currentTime = document.querySelector(".time");
 currentTime.innerHTML = `${timeHour}:${timeMin}`;
+
+let apiKey = "cca34be40btf00a3b62f839750cc64eo";
+let citySearch = document.querySelector(".search-text");
+let city = citySearch.value;
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+
+function Searching(event) {
+  event.preventDefault();
+  let currentCity = document.querySelector(".city");
+  currentCity.innerHTML = `${citySearch.value}`;
+}
+let submitSearch = document.querySelector(".search-button");
+submitSearch.addEventListener("click", Searching);
+axios.get(apiUrl).then(Searching);
